2d4cbb56cdef4eaf35d813dbc2a959fb57f3042e
aliamirah07@gmail.com <40806717+amirah-a@users.noreply.github.com>
2021-11-15 12:52:45 -0400

    Added OrderingStock and Receiving Stock | Added Spinner and Array Adapter | Added Arrays to strings.xml

---
+++ app/src/main/java/com/example/candycorner/OrderingStock.java
@@ -0,0 +1,21 @@
+package com.example.candycorner;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderingStock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiving_stock);
+
+        Spinner spinner = (Spinner) findViewById(R.id.candies);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.option, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
