0b2def910cef7d16c0e7004e51b937e0525675dd
aliamirah07@gmail.com <40806717+amirah-a@users.noreply.github.com>
2021-11-14 23:06:51 -0400

    Added view 2

--- app/src/main/java/com/example/candycorner/ReceivingStocks.java
+++ app/src/main/java/com/example/candycorner/ReceivingStocks.java
@@ -1,2 +1,20 @@
-package com.example.candycorner;public class ReceivingStocks {
+package com.example.candycorner;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReceivingStocks extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stocks_receiving);
+
+        Spinner spinner = (Spinner) findViewById(R.id.candies);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.option, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
 }
