f694633d8beba008d43c0cb6d3736d39cfdd1905
aliamirah07@gmail.com <40806717+amirah-a@users.noreply.github.com>
2021-11-15 13:45:47 -0400

    Fixed resource error | Added View1 and View2 | Implemented Spinner

--- app/src/main/java/com/example/candycorner/MainActivity.java
+++ app/src/main/java/com/example/candycorner/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.candycorner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list = (ListView) findViewById(R.id.menuOptions);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                if (position == 0){
+                    Toast.makeText(getApplicationContext(), "Receiving Stock", Toast.LENGTH_SHORT).show();
+                }
+
+                if (position == 1){
+                    Toast.makeText(getApplicationContext(), "Ordering Stock", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
