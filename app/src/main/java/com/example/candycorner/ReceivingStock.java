1d221c7d30d9b9b8bc0216157924f346ded2cc66
aliamirah07@gmail.com <40806717+amirah-a@users.noreply.github.com>
2021-11-18 09:56:31 -0400

    Completed Database queries for spinner options

--- app/src/main/java/com/example/candycorner/ReceivingStock.java
+++ app/src/main/java/com/example/candycorner/ReceivingStock.java
@@ -1,6 +1,7 @@
 package com.example.candycorner;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -21,6 +22,7 @@ public class ReceivingStock extends AppCompatActivity {
     private SQLiteDatabase db;
     private Cursor cursor;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,38 +42,33 @@ public class ReceivingStock extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
 
                 if (position == 0) {
-                    Toast.makeText(adapterView.getContext(), "Nothing has been selected...", Toast.LENGTH_SHORT).show();
+                    TextView handVal = (TextView) findViewById(R.id.handValue);
+                    TextView transVal = (TextView) findViewById(R.id.transitvalue);
+                    TextView priceVal = (TextView) findViewById(R.id.pricevalue);
+                    TextView quanVal = (TextView) findViewById(R.id.quantityvalue);
+                    TextView amtVal = (TextView) findViewById(R.id.amtvalue);
 
+                    handVal.setText("-");
+                    transVal.setText("-");
+                    priceVal.setText("-");
+                    quanVal.setText("-");
+                    amtVal.setText("-");
                 }
+
                 if (position == 1) {
-                    try{
-                        SQLiteOpenHelper candyCornerDatabaseHelper = new CandyCornerDatabaseHelper(adapter.getContext());
-                        db = candyCornerDatabaseHelper.getWritableDatabase();
+                    itemQuery(adapterView.getContext(), "MilkyWay");
+                }
 
-                        cursor = db.query("Product",
-                                new String[]{"Name", "StockOnHand"},
-                                "Name = ?", new String[]{"Milky Way"},null, null, null);
+                if (position == 2){
+                    itemQuery(adapterView.getContext(), "Gummy Bears");
+                }
 
-                        TextView tv = (TextView) findViewById(R.id.handValue);
-                        int n = cursor.getCount();
+                if (position == 3){
+                    itemQuery(adapterView.getContext(), "Kit Kat");
+                }
 
-                        if (cursor != null && cursor.moveToFirst()){
-                            tv.setText(String.valueOf(cursor.getColumnIndex("Name")));
-                        }
-                        else
-                            tv.setText("Empty");
-                    }
-                    finally {
-                        if (cursor != null) {
-                            cursor.close();
-                        }
-                        db.close();
-                    }
-                    //Toast.makeText(adapterView.getContext(),n, Toast.LENGTH_SHORT).show();
-
-//                    db.close();
-//                    cursor.close();
-
+                if (position == 4){
+                    itemQuery(adapterView.getContext(), "Jolly Ranchers");
                 }
 
             }
@@ -84,5 +81,36 @@ public class ReceivingStock extends AppCompatActivity {
 
         });
     }
+
+    public void itemQuery(Context context, String item){
+        try{
+            SQLiteOpenHelper candyCornerDatabaseHelper = new CandyCornerDatabaseHelper(context);
+            db = candyCornerDatabaseHelper.getWritableDatabase();
+            cursor = db.query("Product",
+                    new String[] {"Name", "StockOnHand", "StockInTransit", "Price", "ReorderQuantity", "ReorderAmount"},
+                    "Name = ?",
+                    new String[] {item},
+                    null, null, null);
+
+            TextView handVal = (TextView) findViewById(R.id.handValue);
+            TextView transVal = (TextView) findViewById(R.id.transitvalue);
+            TextView priceVal = (TextView) findViewById(R.id.pricevalue);
+            TextView quanVal = (TextView) findViewById(R.id.quantityvalue);
+            TextView amtVal = (TextView) findViewById(R.id.amtvalue);
+
+            if (cursor != null && cursor.moveToFirst()){
+                handVal.setText(String.valueOf(cursor.getInt(cursor.getColumnIndex("StockOnHand"))));
+                transVal.setText(String.valueOf(cursor.getInt(cursor.getColumnIndex("StockInTransit"))));
+                priceVal.setText("$" + String.valueOf(cursor.getDouble(cursor.getColumnIndex("Price"))));
+                quanVal.setText(String.valueOf(cursor.getInt(cursor.getColumnIndex("ReorderQuantity"))));
+                amtVal.setText(String.valueOf(cursor.getInt(cursor.getColumnIndex("ReorderAmount"))));
+            }
+        }
+        finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            db.close();
+        }    }
 }
 
